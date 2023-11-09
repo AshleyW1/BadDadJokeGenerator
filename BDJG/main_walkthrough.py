eef41065ccebad2d6e2ea74e1f4c3ff827513ec5
Ashley Watts <charliewatts2004@gmail.com>
2023-11-09 13:06:09 +0000

    made "progress"

--- BDJG/main_walkthrough.py
+++ BDJG/main_walkthrough.py
@@ -1,1 +1,14 @@
 import random
+
+
+#Opens the files needed
+PunchLines = open('assets\PunchLineDict.txt', 'r')
+SetUps = open('assets\SetUpDict.txt', 'r')
+
+SU_Lines = len(SetUps.readlines())
+PL_Lines = len(PunchLines.readlines())
+
+SU_Dict = SetUps.readlines()
+PL_Dict = PunchLines.readlines()
+
+print(SetUps.readlines())
